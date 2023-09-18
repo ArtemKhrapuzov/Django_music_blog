abc601b34774c6cadb664d70e9fe6c1c547492bb
Artem Khrapuzov <tema102@mail.ru>
2023-09-18 13:01:30 +0600

    add register user and login logout

--- blog/music/urls.py
+++ blog/music/urls.py
@@ -4,6 +4,9 @@ from .views import *
 
 urlpatterns = [
     path('', MusicMain.as_view(), name='home'),
+    path('register/', RegisterUser.as_view(), name='register'),
+    path('login/', LoginUser.as_view(), name='login'),
+    path('logout/', logout_user, name='logout'),
     path('post/<slug:post_slug>/', ShowPost.as_view(), name='post'),
     path('category/<slug:cat_slug>/', ShowCat.as_view(), name='cat'),
 ]
--- blog/music/views.py
+++ blog/music/views.py
@@ -1,8 +1,12 @@
+from django.contrib.auth import login, logout
+from django.contrib.auth.views import LoginView
 from django.http import HttpResponse
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
 
 from .models import *
+from .forms import *
 
 
 class MusicMain(ListView):
@@ -11,6 +15,9 @@ class MusicMain(ListView):
     template_name = 'music/index.html'
     context_object_name = 'posts'
     paginate_by = 3
+
+    def get_queryset(self):
+        return Singer.objects.filter(is_published=True).select_related('cat')
 
 
 class ShowPost(DetailView):
@@ -31,3 +38,25 @@ class ShowCat(ListView):
 
     def get_queryset(self):
         return Singer.objects.filter(cat__slug=self.kwargs['cat_slug'], is_published=True).select_related('cat')
+
+
+class RegisterUser(CreateView):
+    """Форма авторизации"""
+    form_class = RegisterUserForm
+    template_name = 'music/auth.html'
+    success_url = reverse_lazy('login')
+
+
+class LoginUser(LoginView):
+    """Форма входа"""
+    form_class = LoginUserForm
+    template_name = 'music/login.html'
+
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+
+def logout_user(request):
+    """Выход"""
+    logout(request)
+    return redirect('login')
